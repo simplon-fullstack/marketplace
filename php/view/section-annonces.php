aedd8bb639a6f09f8dd8321da1049bec050a7be9
Your Name <you@example.com>
2019-09-24 10:34:02 +0200

    Ajout liste annonces: maquette html, css

--- php/view/section-annonces.php
+++ php/view/section-annonces.php
@@ -2,12 +2,35 @@
         <section>
             <h3>Annonces</h3>
             <div class="ligne">
-                <div>COLONNE1</div>
-                <div>COLONNE2</div>
-                <div>COLONNE3</div>
-                <div>COLONNE4</div>
-                <div>COLONNE5</div>
-                <div>COLONNE6</div>
-                <div>COLONNE7</div>
+                <article>
+                    <h3>annonce1</h3>
+                    <img src="assets/images/photo1.jpg" alt="photo1.jpg">
+                    <p>description de l'annonce 1</p>
+                </article>
+                <article>
+                    <h3>annonce2</h3>
+                    <img src="assets/images/photo2.jpg" alt="photo2.jpg">
+                    <p>description de l'annonce 2</p>
+                </article>
+                <article>
+                    <h3>annonce3</h3>
+                    <img src="assets/images/photo3.jpg" alt="photo3.jpg">
+                    <p>description de l'annonce 3</p>
+                </article>
+                <article>
+                    <h3>annonce4</h3>
+                    <img src="assets/images/photo4.jpg" alt="photo4.jpg">
+                    <p>description de l'annonce 4</p>
+                </article>
+                <article>
+                    <h3>annonce5</h3>
+                    <img src="assets/images/photo5.jpg" alt="photo5.jpg">
+                    <p>description de l'annonce 5</p>
+                </article>
+                <article>
+                    <h3>annonce6</h3>
+                    <img src="assets/images/photo6.jpg" alt="photo6.jpg">
+                    <p>description de l'annonce 6</p>
+                </article>
             </div>
         </section>
